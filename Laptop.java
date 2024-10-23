1b9e8a83dcfc81f32336688b01f8aa9e870c6df0
shubhangspd <shubhangpriy@gmail.com>
2024-10-23 14:30:15 +0530

    Laptop.java

--- Laptop.java
+++ Laptop.java
@@ -2,6 +2,7 @@ package Practice;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,11 +15,30 @@ public static void main(String[] args){
 			WebElement searchBox = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
 			searchBox.sendKeys("dell laptop");	
 			driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']")).click();
+			
 			driver.findElement(By.partialLinkText("Laptop Computers | Dell India")).click();
-			driver.findElement(By.partialLinkText("Vostro 3520")).click();
-			//driver.findElement(By.xpath("//section[@id='bottom-navbar']//button[@role='link'][text()='Add to Cart']")).click();
-			driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]")).click();
-			driver.findElement(By.xpath("/html[1]/body[1]/div[5]/div[3]/div[1]/div[2]/button[1]")).click();
+			
+			try 
+			{
+				driver.findElement(By.partialLinkText("Vostro 3520")).click();
+			
+				driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]")).click();
+			
+				driver.findElement(By.xpath("/html[1]/body[1]/div[5]/div[3]/div[1]/div[2]/button[1]")).click();
+			}
+			catch(NoSuchElementException e)
+			{
+				searchBox = driver.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[2]/input[1]"));	
+				searchBox.sendKeys("Vostro 3520");
+				driver.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[2]/div[1]/button[2]")).click();
+				driver.findElement(By.partialLinkText("Vostro 3520")).click();
+				driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]")).click();
+				driver.findElement(By.xpath("/html[1]/body[1]/div[5]/div[3]/div[1]/div[2]/button[1]")).click();
+			}
+			finally 
+			{
+				System.out.println("Done");
+			}
     }
 }
  
